3ed7eecf6d5033e1647d7aa4e93984ce11176584
Marcin Popko <marcinpopko@outlook.com>
2022-02-20 07:47:26 +0100

    Added internal Pi Pico temp measurement.

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ def dht_init():
     pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
     sensor = DHT11(pin)
     return sensor
+def internal_temp():
+    sensor_temp = machine.ADC(4)
+    conversion_factor = 3.3 / (65535)
+    reading = sensor_temp.read_u16() * conversion_factor
+    # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+    # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
+    temperature = 27 - (reading - 0.706)/0.001721
+    print("Internal Pi Pico temp: " + str(temperature) + " °C")
 
 bmp = bmp_init()
 sensor = dht_init()
@@ -32,4 +40,5 @@ t  = (sensor.temperature)
 h = (sensor.humidity)
 print("DHT11 Temperature: {}".format(sensor.temperature) + " °C")
 print("DHT11 Humidity: {}".format(sensor.humidity) + " %")
+internal_temp()
 led.value(0)
