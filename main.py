4af40da506833d1a299ca54475129d4626235c27
Marcin Popko <marcinpopko@outlook.com>
2022-02-23 17:22:24 +0100

    First DHT11 tries on screen SSD1306

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from machine import I2C, Pin
 from sensors import *
 import utime as time
+from ssd1306 import SSD1306_I2C
 
-bmp_sensor = BmpInit()
-dht_sensor = DhtInit()
-led = Pin(25, Pin.OUT)
+def ssd1306Init():
+    WIDTH  = 128                                            # oled display width
+    HEIGHT = 32                                             # oled display height
+    i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=10000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
+    devices = i2c.scan()
+    print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
+    print("I2C Configuration: "+str(i2c))                   # Display I2C config
+    oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)                  # Init oled display
+    time.sleep(1)
+    return oled
 
-led.value(1)
-print("BMP280 Temp: " + str(bmp_sensor.temperature) + " °C")
-print("BMP280 Pressure: " + str(bmp_sensor.pressure/100) + " hPa")
+oled = ssd1306Init() #I2c, SDA pin, SCL pin
+#bmp_sensor = BmpInit(0, 0, 1) #I2c, SDA pin, SCL pin
+dht_sensor = DhtInit(28) #1wire pin
 
-print("DHT11 Temperature: {}".format(dht_sensor.temperature) + " °C")
-print("DHT11 Humidity: {}".format(dht_sensor.humidity) + " %")
-led.value(0)
+while 1:
+    #print("BMP280 Temp: " + str(bmp_sensor.temperature) + " °C")
+    #print("BMP280 Pressure: " + str(bmp_sensor.pressure/100) + " hPa")
+    
+    oled.fill(0)
+    oled.text("Temp: {}".format(dht_sensor.temperature) + " C",5,5)
+    oled.text("rh%: {}".format(dht_sensor.humidity) + " %",5,15)
+    oled.show()
+    time.sleep(1)
--- sensors.py
+++ sensors.py
@@ -3,12 +3,7 @@ from bmp280 import *
 import utime as time
 from dht import DHT11, InvalidChecksum
 
-bmpSdaPin = 0
-bmpSclPin = 1
-bmpI2c = 0
-dht11Pin = 28
-
-def BmpInit():
+def BmpInit(bmpI2c, bmpSdaPin, bmpSclPin):
     bus = I2C(bmpI2c, sda=Pin(bmpSdaPin), scl=Pin(bmpSclPin)) 
     bmp = BMP280(bus)
     bmp.use_case(BMP280_CASE_WEATHER)
@@ -20,7 +15,7 @@ def BmpInit():
     bmp.spi3w = BMP280_SPI3W_ON
     bmp.power_mode = BMP280_POWER_NORMAL
     return bmp
-def DhtInit():
-    pin = Pin(dht11_pin, Pin.OUT, Pin.PULL_DOWN)
+def DhtInit(dht11Pin):
+    pin = Pin(dht11Pin, Pin.OUT, Pin.PULL_DOWN)
     sensor = DHT11(pin)
     return sensor
